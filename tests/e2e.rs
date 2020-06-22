05a9b718b445895bafb2d19b475d322dfd3d3dbc
Bartosz Zbytniewski <bartosz.zbytniewski.dev@gmail.com>
2020-06-22 18:06:58 +0200

    Add real text to check

--- tests/e2e.rs
+++ tests/e2e.rs
@@ -4,13 +4,24 @@ use assert_gui::GuiError;
 const FIRST_ANSWER: (f64, f64) = (757.0, 321.0);
 const NEXT_BUTTON: (f64, f64) = (1151.0, 1032.0);
 
+const QUESTION_TXT: &str = r#"A developer is working on a complex project with multiple bundles. One bundle provides an OSGi service for other bundles. Which two options are necessary to ensure that the other bundles can reference that OSGi service? (Choose two.)
+
+The bundles consuming the service need to import the fully qualified name of the service interface.
+The service needs to correctly declare metatype information.
+The bundle providing the service needs to contain a whitelist of allowed consumer bundles.
+The bundle providing the service needs to contain an adequate SCR descriptor file.
+The bundle providing the service needs to export the java package of the service interface.
+"#;
+
 #[test]
 fn test() -> Result<(), GuiError> {
     Gui::bin("quizers")?
         .open()
         .click(FIRST_ANSWER)?
         .click(NEXT_BUTTON)?
-        .assert_text("Test");
+        .assert()
+        .with_similarity(0.75)
+        .text(QUESTION_TXT)?;
 
     Ok(())
 }
